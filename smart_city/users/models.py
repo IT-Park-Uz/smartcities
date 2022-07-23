375d591c720f31d8a0cb50d48fe497d741bc45ef
Ergashev Baxtiyor <86454228+Baxtiyor98@users.noreply.github.com>
2022-07-23 15:59:26 +0500

    add

--- smart_city/users/models.py
+++ smart_city/users/models.py
@@ -6,28 +6,11 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='Users', null=True, blank=True)
-    organization = models.ForeignKey('Organization', on_delete=models.SET_NULL, null=True, blank=True,
-                                     verbose_name=_('Organization'), related_name='users')
+    email = models.EmailField(_('email address'), unique=True)
+    organization_name = models.CharField(max_length=70, verbose_name=_('Organization'), null=True, blank=True)
+    work_name = models.CharField(_('work name'), max_length=255, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True, verbose_name=_('Bio'))
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
 
-
-class Organization(models.Model):
-    organization_name = models.CharField(max_length=50, verbose_name=_('Organization'))
-    image = models.ImageField(upload_to='Organization', null=True, blank=True, verbose_name=_('Image'))
-    bio = models.TextField(null=True, blank=True, verbose_name=_('Bio'))
-    is_active = models.BooleanField(default=False, verbose_name=_('Is Active'))
-
-    class Meta:
-        ordering = ['-id']
-
-    @property
-    def imageURL(self):
-        try:
-            return self.image.url
-        except:
-            return ''
-
-    def __str__(self):
-        return f"{self.id}| {self.organization_name[:20]}"

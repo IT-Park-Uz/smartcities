1101a6a80fade8d19206572ade08fc796e61d455
qayrat-sultan <productgsm@gmail.com>
2022-08-10 08:06:44 +0000

    server cors changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -70,6 +70,7 @@ DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # no
 # AWS_ACCESS_KEY_ID = env("DJANGO_AWS_ACCESS_KEY_ID")
 # # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 # AWS_SECRET_ACCESS_KEY = env("DJANGO_AWS_SECRET_ACCESS_KEY")
+CORS_ALLOW_ALL_ORIGINS = True
 # # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
 # AWS_STORAGE_BUCKET_NAME = env("DJANGO_AWS_STORAGE_BUCKET_NAME")
 # # https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings

5bd92dc72c6b6918c6631dfd736527d46f6a759e
Kairat Sultan <garri.sultonov@mail.ru>
2022-11-06 00:40:35 +0600

    auto generate sitemap from /sitemap.xml

---
+++ smart_city/posts/sitemaps.py
@@ -0,0 +1,47 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Article, News, Question
+
+
+class ArticleSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.6
+    protocol = 'https'
+
+    def items(self):
+        return Article.objects.filter(is_active=True)
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, obj):
+        return '/article/%s' % obj.id
+
+
+class NewsSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.6
+    protocol = 'https'
+
+    def items(self):
+        return News.objects.filter(is_active=True)
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, obj):
+        return '/news/%s' % obj.id
+
+
+class QuestionSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return Question.objects.filter(is_active=True)
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, obj):
+        return '/question/%s' % obj.id
